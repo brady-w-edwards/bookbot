7f85ff718b7041c3969a0ad893bbe595637a35df
Brady Edwards <bwe731@gmail.com>
2024-11-18 17:47:43 -0600

    added character dictionary function

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     bookPath = "books/frankenstein.txt"
     text = getBookText(bookPath)
     wordCount = countWords(text)
+    letterCount = countCharacters(text)
     print(f"The Frankenstein book contains {wordCount} words!")
+    print(letterCount)
 
 def getBookText(path):
     with open(path) as f:
@@ -11,5 +13,21 @@ def getBookText(path):
 def countWords(text):
     wordsArray = text.split()
     return len(wordsArray)
+
+# def countCharacters(text):
+#     characterList = text.strip()
+#     lowercaseList = characterList.lower()
+#     return len(lowercaseList)
+
+def countCharacters(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
 
 main()
