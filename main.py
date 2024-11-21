13ec1f77f9f98cbc367ddb9bda3b217fde3df95b
Brady Edwards <bwe731@gmail.com>
2024-11-20 23:35:59 -0600

    changed variables and functions to snake case

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 def main():
-    bookPath = "books/frankenstein.txt"
-    text = getBookText(bookPath)
-    wordCount = countWords(text)
-    letterCount = countCharacters(text)
-    print(f"The Frankenstein book contains {wordCount} words!")
-    print(letterCount)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = count_words(text)
 
-def getBookText(path):
+    character_count = char_dictionary(text)
+    letter_count = [character_count]
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} found in the document\n")
+    for letter in letter_count:
+        if letter.isalpha():
+            number = letter_count[letter]
+            print(f"The {letter} was found {number} times in the document")
+
+def get_book_text(path):
     with open(path) as f:
         return f.read()
         
-def countWords(text):
-    wordsArray = text.split()
-    return len(wordsArray)
+def count_words(text):
+    words_array = text.split()
+    return len(words_array)
 
 # def countCharacters(text):
 #     characterList = text.strip()
 #     lowercaseList = characterList.lower()
 #     return len(lowercaseList)
 
-def countCharacters(text):
+def char_dictionary(text):
     chars = {}
     for c in text:
         lowered = c.lower()
